2be601e399a86eb65c737859886a8e7ad7f58d2b
RajeswariPounraj <raji.eie97@gmail.com>
2021-05-18 13:17:39 +0530

    feature(BLAZ-12508): Diagram Builder WASM

---
+++ interop.js
@@ -0,0 +1,55 @@
+﻿function diagramNameChange(args, isSet) {
+    if (isSet) {
+        document.getElementById('diagramName').innerHTML = args;
+    }
+    else {
+        document.getElementById('diagramName').innerHTML = document.getElementById('diagramEditable').value;
+        document.getElementsByClassName('db-diagram-name-container')[0].classList.remove('db-edit-name');
+        document.getElementById("exportfileName").value = document.getElementById('diagramName').innerHTML;
+    }
+}
+
+UtilityMethods_hideElements = function (elementType, diagramType) {
+    var diagramContainer = document.getElementsByClassName('diagrambuilder-container')[0];
+    if (diagramContainer.classList.contains(elementType)) {
+        if (!(diagramType === 'mindmap-diagram' || diagramType === 'orgchart-diagram')) {
+            diagramContainer.classList.remove(elementType);
+        }
+    }
+    else {
+        diagramContainer.classList.add(elementType);
+    }
+};
+
+function hideMenubar() {
+    UtilityMethods_hideElements('hide-menubar');
+}
+
+function getHyperLinkValueFromDocument(id, attribute) {
+    return document.getElementById(id).value;
+}
+
+function click() {
+    document.getElementById('UploadFiles').click();
+}
+
+function hideElements(elementType) {
+    var diagramContainer = document.getElementsByClassName('diagrambuilder-container')[0];
+    if (diagramContainer.classList.contains(elementType)) {
+        diagramContainer.classList.remove(elementType);
+    } else {
+        diagramContainer.classList.add(elementType);
+    }
+}
+
+function click() {
+    document.getElementById('defaultfileupload').click();
+}
+function loadFile(file) {
+    var base64 = file.rawFile.replace("data:application/json;base64,", "");
+    var json = atob(base64)
+    return json;
+}
+function loadDiagram(event) {
+    return event.target.result.toString();
+}
